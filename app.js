5c9a17988ab094b5f864f404327da0b3c890389d
kawinpromsopa <44109187+kawinpromsopa@users.noreply.github.com>
2018-10-14 12:06:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 const config = require('./config.json')
 
 app.get('/', function (req, res) {
-  res.send(config.message||'Hello World!')
+  res.send(config.message||'Hello World, This is a Devops task from Ekoapp by kawinpromsopa!')
 })
 
 app.listen(config.port, function () {
